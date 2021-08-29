2ef37095afad7380d8f12af2e29426a117b0ce71
Evan Pratten <ewpratten@gmail.com>
2021-08-29 12:52:34 -0400

    Main lib code

--- README.md
+++ README.md
@@ -1,17 +1,9 @@
-# <repo_name>
-[![Crates.io](https://img.shields.io/crates/v/<crate_name>)](https://crates.io/crates/<crate_name>) 
-[![Docs.rs](https://docs.rs/<crate_name>/badge.svg)](https://docs.rs/<crate_name>) 
-[![Build](https://github.com/Ewpratten/<repo_name>/actions/workflows/build.yml/badge.svg)](https://github.com/Ewpratten/<repo_name>/actions/workflows/build.yml)
-[![Clippy](https://github.com/Ewpratten/<repo_name>/actions/workflows/clippy.yml/badge.svg)](https://github.com/Ewpratten/<repo_name>/actions/workflows/clippy.yml)
-[![Audit](https://github.com/Ewpratten/<repo_name>/actions/workflows/audit.yml/badge.svg)](https://github.com/Ewpratten/<repo_name>/actions/workflows/audit.yml)
+# imgur2018
+[![Crates.io](https://img.shields.io/crates/v/imgur2018)](https://crates.io/crates/imgur2018) 
+[![Docs.rs](https://docs.rs/imgur2018/badge.svg)](https://docs.rs/imgur2018) 
+[![Build](https://github.com/Ewpratten/imgur2018/actions/workflows/build.yml/badge.svg)](https://github.com/Ewpratten/imgur2018/actions/workflows/build.yml)
+[![Clippy](https://github.com/Ewpratten/imgur2018/actions/workflows/clippy.yml/badge.svg)](https://github.com/Ewpratten/imgur2018/actions/workflows/clippy.yml)
+[![Audit](https://github.com/Ewpratten/imgur2018/actions/workflows/audit.yml/badge.svg)](https://github.com/Ewpratten/imgur2018/actions/workflows/audit.yml)
 
 
-repo description
-
-## Installation
-
-This crate can be installed via `cargo` with:
-
-```sh
-cargo install <crate_name>
-```
+Other Rust Imgur libraries use very old APIs. This is just a simple upload client with `.await` support.
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,59 @@
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error(transparent)]
+    ReqwestErr(#[from] reqwest::Error),
+
+    #[error(transparent)]
+    UrlParseErr(#[from] url::ParseError),
+
+    #[error("Failed to parse imgur's json data")]
+    JsonLinkParseErr,
+}
+
+/// Upload some data to imgur
+pub async fn imgur_upload(client_id: &str, data: Vec<u8>) -> Result<url::Url, Error> {
+    // Make post request to imgur
+    let response = reqwest::ClientBuilder::default()
+        .build()?
+        .post("https://api.imgur.com/3/image")
+        .header("Authorization", format!("Client-ID {}", client_id))
+        .body(data)
+        .send()
+        .await?;
+
+    // Parse response
+    let response_json: serde_json::Value = response.json().await?;
+    Ok(url::Url::parse(
+        response_json["data"]["link"]
+            .as_str()
+            .ok_or(Error::JsonLinkParseErr)?,
+    )?)
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_imgur_upload() {
+        tokio_test::block_on(async {
+            let url = imgur_upload(
+                "332741bbdcde865",
+                reqwest::get("https://www.rust-lang.org/logos/rust-logo-128x128.png")
+                    .await
+                    .unwrap()
+                    .bytes()
+                    .await
+                    .unwrap()
+                    .iter()
+                    .map(|x| *x)
+                    .collect(),
+            )
+            .await
+            .unwrap();
+            println!("{:?}", url.to_string());
+        });
     }
 }
